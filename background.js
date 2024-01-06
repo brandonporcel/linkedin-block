9bee701fa7a4874dc9da966c76c1a030a1ac4bfc
brandon <brandon7.7porcel@gmail.com>
2024-01-06 15:23:39 -0300

    feat: eliminacion Recommended for you posts

--- background.js
+++
@@ -1,9 +0,0 @@
-chrome.runtime.onInstalled.addListener(function () {
-  chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-    if (changeInfo.status === "complete") {
-      chrome.tabs.sendMessage(tabId, {
-        message: "TabUpdated",
-      });
-    }
-  });
-});
--- main.js
+++ main.js
@@ -1,64 +1,60 @@
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  if (request.message !== "TabUpdated") return;
-
-  function getParentByHierarchy(element, levels) {
-    let parent = element;
-    for (let i = 0; i < levels; i++) {
-      parent = parent?.parentNode;
-    }
-    return parent;
+function getParentByHierarchy(element, levels) {
+  let parent = element;
+  for (let i = 0; i < levels; i++) {
+    parent = parent?.parentNode;
   }
+  return parent;
+}
 
-  function hideElement(element, displayValue = "none") {
-    if (element) {
-      element.style.display = displayValue;
-    }
+function hideElement(element, displayValue = "none") {
+  if (element) {
+    element.style.display = displayValue;
   }
+}
 
-  function containsText(element, text) {
-    return element.innerText.includes(text);
-  }
+function containsText(element, text) {
+  return element.innerText.includes(text);
+}
 
-  function hideSuggestedPosts() {
-    const headers = document.querySelectorAll(".update-components-header");
-    headers.forEach((header) => {
-      const spans = header.querySelectorAll("span");
-      spans.forEach((span) => {
-        if (
-          containsText(span, "Suggested") ||
-          containsText(span, "Jobs recommended for you") ||
-          containsText(span, "Expert answers on")
-        ) {
-          const fatherElem = getParentByHierarchy(span, 6);
-          hideElement(fatherElem);
-        }
-      });
-    });
-  }
-
-  function hidePromos() {
-    const promos = document.querySelectorAll(".update-components-promo");
-
-    promos.forEach((promoElem) => {
-      hideElement(promoElem);
-    });
-  }
-
-  function handleScroll() {
-    if (location.href !== "https://www.linkedin.com/feed/") return;
-
-    const spans = document.querySelectorAll("span");
-
+function hideSuggestedPosts() {
+  const headers = document.querySelectorAll(".update-components-header");
+  headers.forEach((header) => {
+    const spans = header.querySelectorAll("span");
     spans.forEach((span) => {
-      if (containsText(span, "Promoted")) {
-        const fatherElem = getParentByHierarchy(span, 5);
+      if (
+        containsText(span, "Suggested") ||
+        containsText(span, "Jobs recommended for you") ||
+        containsText(span, "Expert answers on") ||
+        containsText(span, "Recommended for you")
+      ) {
+        const fatherElem = getParentByHierarchy(span, 6);
         hideElement(fatherElem);
       }
     });
+  });
+}
 
-    hideSuggestedPosts();
-    hidePromos();
-  }
+function hidePromos() {
+  const promos = document.querySelectorAll(".update-components-promo");
 
-  window.addEventListener("scroll", handleScroll);
-});
+  promos.forEach((promoElem) => {
+    hideElement(promoElem);
+  });
+}
+
+function handleScroll() {
+  const spans = document.querySelectorAll("span");
+
+  spans.forEach((span) => {
+    if (containsText(span, "Promoted")) {
+      const fatherElem = getParentByHierarchy(span, 5);
+      hideElement(fatherElem);
+    }
+  });
+
+  hideSuggestedPosts();
+  hidePromos();
+}
+
+window.addEventListener("scroll", handleScroll);
+document.addEventListener("DOMContentLoaded", handleScroll);
